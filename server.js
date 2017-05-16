e4aaabe64354f688801bc2f9234f9ff5d4ffd243
Sherwin Hulley <Sherwin Hulley>
2017-05-16 11:24:09 +0200

    added some authentication

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app = express()
 var bodyParser =  require('body-parser');
 var port = process.env.NODE_PORT | 8080;
 
+//auth key
+var key = "aa84e0cc54ceb040f7f6bc364d6a7782";
+
 app.use(bodyParser.json());
 
 app.get('/', function(req,res) {
@@ -39,13 +42,14 @@ pool.connect(function (err, client) {
 		});
 	});
 
-	client.query('CREATE TABLE events (source TEXT, event TEXT, created TIMESTAMP, data JSONB)', function (err, result) {
-		if (err) {
-			console.log('table exist');
-			return;
-		}
-		console.log('table created');
-		client.end(function (err) {
+	client.query('CREATE TABLE events (source TEXT, event TEXT, created TIMESTAMP, data JSONB)',
+		function (err, result) {
+			if (err) {
+				console.log('table exist');
+				return;
+			}
+			console.log('table created');
+			client.end(function (err) {
 			if (err) throw err;
 		});
 	});
@@ -57,8 +61,9 @@ pool.connect(function (err, client) {
 //log this into a postgres 
 app.post('/event', function (req, res) {
 	var body = req.body;
-	res.setHeader('Content-Type', 'application/json');
-	console.log(body.source);
+	if (req.headers.authentication !== key) {
+		res.status(401).end();
+	}
 	pool.connect(function (err, client) {
 		if (err) throw err;
 		client.query('INSERT INTO events (source, event, created, data) VALUES ($1, $2, $3, $4)',
